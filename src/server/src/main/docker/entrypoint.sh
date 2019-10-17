43e285dd071efc811073c4ba93362e45b23a0e41
leleueri <eric.leleu.dev@gmail.com>
2019-10-17 10:00:11 +0200

    Update Dockerfile to build Google Marketplace Image

--- src/server/src/main/docker/entrypoint.sh
+++ src/server/src/main/docker/entrypoint.sh
@@ -18,12 +18,12 @@ if [ "$1" = 'cassandra-reaper' ]; then
     set -x
 
     # get around `/usr/local/bin/configure-persistence.sh: line 65: can't create /etc/cassandra-reaper.yml: Interrupted system call` unknown error
-    touch /etc/cassandra-reaper.yml
+    # touch /etc/cassandra-reaper.yml
 
-    su-exec reaper /usr/local/bin/configure-persistence.sh
-    su-exec reaper /usr/local/bin/configure-webui-authentication.sh
-    su-exec reaper /usr/local/bin/configure-metrics.sh
-    exec su-exec reaper java \
+    /usr/local/bin/configure-persistence.sh
+    /usr/local/bin/configure-webui-authentication.sh
+    /usr/local/bin/configure-metrics.sh
+    java \
                     ${JAVA_OPTS} \
                     -jar /usr/local/lib/cassandra-reaper.jar server \
                     /etc/cassandra-reaper.yml
